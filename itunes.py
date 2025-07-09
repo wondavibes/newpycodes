92cdba1d5353bb537bf090730613d56d40285bfd
wondavibes <amosayemoro2001@gmail.com>
2025-07-09 14:27:45 +0100

    new edits

--- itunes.py
+++ itunes.py
@@ -10,10 +10,8 @@ response = requests.get("https://itunes.apple.com/search?entity=song&limit=5&ter
 o = response.json()
 for result in o['results']:
     print(result['artistName'], result['trackName'], result['collectionName'], result['releaseDate'])
-    """print("Artist Name:", result['artistName'])
-    print("Album Name:", result['collectionName'])
-    print("Release Date:", result['releaseDate'])
-    print("Track Price:", result['trackPrice'])
+  
+    """print("Track Price:", result['trackPrice'])
     print("Collection Price:", result['collectionPrice'])
     print("Track Time:", result['trackTimeMillis'])
     print("Country:", result['country'])
