c5d05c5fbd800b3bf20200d772290a7e1cb96d39
wondavibes <amosayemoro2001@gmail.com>
2025-08-31 13:47:40 +0100

    new bank functions

--- bank.py
+++ bank.py
@@ -1,13 +1,16 @@
 class Acct:
     def __init__(self):
-        self._balance = 0
+        self._balance:int = 0
     
     def __str__(self) :
-        return f"Your current account balance is: {self._balance}"
+        return f"Your current account balance is: ${self._balance}"
 
         
     def __add__(self,other):
-        ...
+        if isinstance(other, Acct):
+            return f"${self._balance + other._balance}"
+        else:
+            raise TypeError("Both operands must be Acct instances")
 
 
 #    @property
@@ -15,19 +18,28 @@ class Acct:
 #        return self._balance
 
 
-    def deposit(self, n):
+    def deposit(self, n:int):
         self._balance += n
 
 
     def withdraw(self, n):
         self._balance -= n
 
+    def transfer(self, other, n):
+        self._balance -= n
+        other._balance += n
+
+        
 def main():
     account = Acct()
+    demo = Acct()
     print(account)
     account.deposit(180)
     account.withdraw(60)
+    account.transfer(demo,70)
     print(account)
+    print(f"In demo {demo}")
+    print("The total balance is",demo+account, sep=None)
 
 
 if __name__ == "__main__":
