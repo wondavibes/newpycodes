60b4ef4d58b8c8a5c79ee1e8c661dd85ca6dd203
wondavibes <amosayemoro2001@gmail.com>
2025-10-05 19:25:50 +0100

    new files

--- names.py
+++ names.py
@@ -4,5 +4,5 @@ with open("names.txt") as file:
     for line in file:
         names.append(line.rstrip()) #r.strip is necesssary to avoid double spaces between print statements
 
-for name in sorted(names, reverse=True):
+for name in sorted(names, reverse=False):
     print(f"hello, {name}")
